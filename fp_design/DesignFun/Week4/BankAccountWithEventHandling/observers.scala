098cf41c4d031e0a0b7c3129736259c659cd66ab
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-04-24 11:56:33 -0500

    reactive publish/subscribe example

--- fp_design/DesignFun/Week4/BankAccountWithEventHandling/observers.scala
+++ fp_design/DesignFun/Week4/BankAccountWithEventHandling/observers.scala
@@ -4,5 +4,9 @@ object observers extends App {
   val a = new BankAccount
   val b = new BankAccount
   val consolidator: Consolidator = new Consolidator(List(a, b))
-  consolidator.totalBalance
+  println(consolidator.totalBalance)
+  a deposit 30
+  println(consolidator.totalBalance)
+  b deposit 20
+  println(consolidator.totalBalance)
 }
