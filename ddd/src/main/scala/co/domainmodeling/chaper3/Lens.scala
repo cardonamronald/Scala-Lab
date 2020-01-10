b0ab9088705154367d522f0b3b2caf7cd28fd2fd
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2020-01-09 19:33:50 -0500

    Add scala formatter and rename type parameters

--- ddd/src/main/scala/co/domainmodeling/chaper3/Lens.scala
+++ ddd/src/main/scala/co/domainmodeling/chaper3/Lens.scala
@@ -1,13 +1,15 @@
 package co.domainmodeling.chaper3
 
-case class Lens[Object, Value] (
-                        get: Object => Value,
-                        set: (Object, Value) => Object
+case class Lens[O, V] (
+                        get: O => V,
+                        set: (O, V) => O
                       )
 object Lens {
-  def compose[Outer, Inner, Value](outer: Lens[Outer, Inner], inner: Lens[Inner, Value]): Lens[Outer, Value] =
+  def compose[Outer, Inner, Value](
+                                    outer: Lens[Outer, Inner],
+                                    inner: Lens[Inner, Value]): Lens[Outer, Value] =
     Lens[Outer, Value] (
-      get = outer.get andThen inner.get,
+      get = obj => inner.get(outer.get(obj)),
       set = (obj, value) => outer.set(obj, inner.set(outer.get(obj), value))
     )
 }
